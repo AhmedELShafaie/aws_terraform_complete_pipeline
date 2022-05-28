5d825be00a2d2995567a80afbf4957491cb4752d
Ahmed <ahmed.fathy@gmail.com>
2022-05-28 17:44:26 +0200

    Add Docker install to userdata (#10)
    Co-authored-by: Ahmed <ahmed@loc.kr>

--- terraform_scripts/main.tf
+++ terraform_scripts/main.tf
@@ -68,10 +68,18 @@ resource "aws_instance" "web" {
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
+              sudo apt-get remove docker docker-engine docker.io containerd runc
+              sudo apt-get install ca-certificates curl lsb-release
+              sudo mkdir -p /etc/apt/keyrings
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+              echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update
+              sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+              sudo systemctl enable docker && sudo systemctl start docker
+              sudo usermod -a -G docker ubuntu
+              sudo docker run hello-world
               EOF
 }
 
