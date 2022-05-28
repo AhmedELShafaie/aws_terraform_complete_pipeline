978c7f7d3492331d351651eaef26a82c3a7410f0
Ahmed <ahmed.fathy@gmail.com>
2022-05-28 02:16:16 +0200

    Add new output for server address only Without Port (#6)
    Co-authored-by: Ahmed <ahmed@loc.kr>

--- terraform_scripts/main.tf
+++ terraform_scripts/main.tf
@@ -102,3 +102,8 @@ resource "aws_security_group" "web-sg" {
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
 }
+
+output "server-address" {
+  value = "${aws_instance.web.public_dns}"
+}
+
