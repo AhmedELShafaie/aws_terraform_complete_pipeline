6c4d5d4e2ffec184bd2a3a91ebfd36c40fc31d97
Ahmed <ahmed.fathy@gmail.com>
2022-05-28 20:18:45 +0200

    Update main.tf

--- terraform_scripts/main.tf
+++ terraform_scripts/main.tf
@@ -69,14 +69,10 @@ resource "aws_instance" "web" {
               #!/bin/bash
               set -ex
               apt-get update
-              sudo apt-get remove docker docker-engine docker.io containerd runc
-
               sudo apt-get install -y ca-certificates curl lsb-release
-
               sudo mkdir -p /etc/apt/keyrings
               curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-              echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+              echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
               sudo apt-get update
               sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
